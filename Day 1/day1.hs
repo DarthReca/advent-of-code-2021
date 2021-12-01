f50b30643bb121d7a0a2e4066c68219e72aa267d
Daniele R.C <darthreca@gmail.com>
2021-12-01 23:22:09 +0100

    Day1

---
+++ Day 1/day1.hs
@@ -0,0 +1,21 @@
+module Main where
+parseFile :: IO [Integer]
+parseFile = do
+    contents <- readFile "input.txt"
+    let fileLines = map read $ lines contents :: [Integer]
+    return fileLines
+
+twoSlidingWindow :: [Integer] -> Int 
+twoSlidingWindow depths = length . filter (\x -> snd x > fst x) . zip depths $ drop 1 depths
+
+
+threeSlidingSum :: [Integer] -> [Integer]
+threeSlidingSum depths = map (\(x,y,z) -> x + y + z) $ zip3 depths depths1 depths2
+    where 
+        depths1 = drop 1 depths
+        depths2 = drop 2 depths
+
+main :: IO ()
+main = do 
+    depths <- parseFile
+    print . twoSlidingWindow $ threeSlidingSum depths 
