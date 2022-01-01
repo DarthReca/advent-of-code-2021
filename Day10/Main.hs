a8f275617e96dd44bc4a335ee877183e5823db2c
Daniele R.C <darthreca@gmail.com>
2022-01-01 15:55:49 +0100

    Day10

--- Day10/Main.hs
+++ Day10/Main.hs
@@ -2,10 +2,17 @@ module Day10.Main where
 import           Data.Either                    ( fromLeft
                                                 , fromRight
                                                 , isLeft
+                                                , isRight
                                                 )
-import           Data.List
-import           Data.Maybe
+import           Data.List                      ( find
+                                                , sort
+                                                )
+import           Data.Map                       ( alterF )
+import           Data.Maybe                     ( fromJust
+                                                , isNothing
+                                                )
 
+parseFile :: IO [String]
 parseFile = do
   contents <- readFile "input.txt"
   let result = lines contents
@@ -18,14 +25,24 @@ openingChar '}' = '{'
 openingChar '>' = '<'
 openingChar _   = ' '
 
-score :: Maybe (Either Char Char) -> Int
-score x | isNothing x            = 0
-        | fromJust x == Left '(' = 3
-        | fromJust x == Left '[' = 57
-        | fromJust x == Left '{' = 1197
-        | fromJust x == Left '<' = 25137
-        | otherwise              = 0
+errorScore :: Maybe (Either Char Char) -> Int
+errorScore x | isNothing x            = 0
+             | fromJust x == Left '(' = 3
+             | fromJust x == Left '[' = 57
+             | fromJust x == Left '{' = 1197
+             | fromJust x == Left '<' = 25137
+             | otherwise              = 0
 
+autocompleteScore :: Either Char Char -> Int
+autocompleteScore (Right '(') = 1
+autocompleteScore (Right '[') = 2
+autocompleteScore (Right '{') = 3
+autocompleteScore (Right '<') = 4
+autocompleteScore _           = 0
+
+
+-- |This is like a queue with automated push or pop.
+-- Return the queue with with a pushed Right in case of opening paranthesis or a popped Right in case of closing parenthesis or a pushed Left in case of error
 pushOrPop :: [Either Char Char] -> Char -> [Either Char Char]
 pushOrPop queue element
   | openingChar element == ' ' = queue ++ [Right element]
@@ -34,6 +51,11 @@ pushOrPop queue element
 
 main = do
   contents <- parseFile
-  let syntaxErrorScore =
-        sum $ map (score . find isLeft . foldl pushOrPop []) contents
+  let rightAndLefts    = map (foldl pushOrPop []) contents
+  let syntaxErrorScore = sum $ map (errorScore . find isLeft) rightAndLefts
   print syntaxErrorScore
+  -- Part 2
+  let incompletes = filter (all isRight) rightAndLefts
+  let incompletesScores = sort
+        $ map (foldr (\x acc -> acc * 5 + autocompleteScore x) 0) incompletes
+  print $ incompletesScores !! (length incompletesScores `quot` 2)
